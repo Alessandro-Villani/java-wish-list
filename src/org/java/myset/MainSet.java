238e5c5de4091abb3c0e06de1e0bbf6b019bb928
Alessandro-Villani <alessvill@libero.it>
2023-05-12 12:43:59 +0200

    added MainSet

---
+++ src/org/java/myset/MainSet.java
@@ -0,0 +1,26 @@
+package org.java.myset;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class MainSet {
+
+	public static void main(String[] args) {
+
+
+		Random rnd = new Random();
+		Set<Integer> randomNumbers = new HashSet<>();
+		
+		while(randomNumbers.size() < 5) {
+			int random = rnd.nextInt(2, 12);
+			randomNumbers.add(random);
+		}
+		
+		for(Integer num : randomNumbers) {
+			System.out.println(num);
+		}
+
+	}
+
+}
